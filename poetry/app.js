e16b46b6e61aaf0b7dbec9b64d429af56069e58f
pearlmalt <69867561+pearlmalt@users.noreply.github.com>
2021-06-07 20:20:51 -0700

    Commit
    A poetry blog space! To share your favorite poems and showcase new content.

---
+++ poetry/app.js
@@ -0,0 +1,71 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+
+const homeStartingContent = "Hello! Here is a neat blog site to upload poems. Please submit peoms by others and also orginial content.";
+const aboutContent = "Hi there! My name is Pearl and I am learning to code. I live in the Bay Area. I love flowers, poetry and tea. Looking forward to connecting with the peotry community.";
+const contactContent = "maltpearl @ gmail.com";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+let posts = [];
+
+app.get("/", function(req, res){
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+    });
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+
+});
+
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  });
+
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
